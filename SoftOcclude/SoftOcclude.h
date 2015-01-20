e90bf1d473883ffe81c235398f781a9366b25746
Andrew Mac <andrewmac@gmail.com>
2015-01-19 23:31:56 -0400

    Copyright fix + minor cleanup.

--- SoftOcclude/SoftOcclude.h
+++ SoftOcclude/SoftOcclude.h
@@ -1,19 +1,24 @@
-//--------------------------------------------------------------------------------------
-// Copyright 2015 Andrew Mac
-// All Rights Reserved
+//-----------------------------------------------------------------------------
+// Copyright (c) 2015 Andrew Mac
 //
-// Permission is granted to use, copy, distribute and prepare derivative works of this
-// software for any purpose and without fee, provided, that the above copyright notice
-// and this statement appear in all copies.  Intel makes no representations about the
-// suitability of this software for any purpose.  THIS SOFTWARE IS PROVIDED "AS IS."
-// INTEL SPECIFICALLY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, AND ALL LIABILITY,
-// INCLUDING CONSEQUENTIAL AND OTHER INDIRECT DAMAGES, FOR THE USE OF THIS SOFTWARE,
-// INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PROPRIETARY RIGHTS, AND INCLUDING THE
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  Intel does not
-// assume any responsibility for any errors which may appear in this software nor any
-// responsibility to update it.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//--------------------------------------------------------------------------------------
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//-----------------------------------------------------------------------------
 
 #include "Rasterizers/AABBoxRasterizerScalarST.h"
 #include "Rasterizers/DepthBufferRasterizerScalarST.h"
@@ -60,7 +65,6 @@ class SoftOcclusionTest
       UINT GetNumOccluderTris() { return mpDBR->GetNumTriangles(); }
       
       // Occludee (mpAABB) Functions
-      //void CreateTransformedAABBoxes(CPUTAssetSet **pAssetSet, UINT numAssetSets) { mpAABB->CreateTransformedAABBoxes(pAssetSet, numAssetSets); }
       TransformedAABBoxScalar* AddOccludee() { return mpAABB->AddOccludee(); }
       void RefreshOccludees() { mpDBR->RefreshOccluders(); }
       void SetOccludeeSizeThreshold(float occludeeSizeThreshold){ mpAABB->SetOccludeeSizeThreshold(occludeeSizeThreshold); }
